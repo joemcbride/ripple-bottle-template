5e4247cdd8962f22288dcf62ccb4109aa94c4b13
Joe McBride <joe@uicraftsman.com>
2013-05-08 17:10:44 -0600

    Initial commit

---
+++ FUBUPROJECTNAME/FUBUPROJECTSHORTNAMERegistry.cs
@@ -0,0 +1,11 @@
+﻿using FubuMVC.Core;
+
+namespace FUBUPROJECTNAME
+{
+    public class FUBUPROJECTSHORTNAMERegistry : IFubuRegistryExtension
+    {
+        public void Configure(FubuRegistry registry)
+        {
+        }
+    }
+}
